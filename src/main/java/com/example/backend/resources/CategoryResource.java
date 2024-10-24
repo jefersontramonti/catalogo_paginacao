43e42865d440c51bb948960895bfd7cba5c015a8
Jeferson Tramonti <jefersontramonti@outlook.com>
2024-10-23 23:03:06 -0300

    initial project

--- src/main/java/com/example/backend/resources/CategoryResource.java
+++ src/main/java/com/example/backend/resources/CategoryResource.java
@@ -11,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
